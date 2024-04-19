7f9bb066df75a7e2b7f18d2b54b8f304d237c820
yuto0807 <tsuboi0831@gmail.com>
2024-04-19 01:17:16 +0000

    dockerでreact✖︎typescriptの環境構築

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# life-management-frontend
+# 実装予定
+・時間管理 ・タスク管理 ・資産管理 ・職歴管理 ・技能管理
 
-## 実装予定
-
-・時間管理
-・資産管理
-・職歴管理
-・技能管理
+## TODO
+・formatterの設定
+・Github Actionsの設定
+・Dockerの設定確認
+・README.md更新
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>aa
+    </>
+  )
+}
+
+export default App
